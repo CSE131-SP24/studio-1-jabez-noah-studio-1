11c7eb17374b6b67490cd63faf59e00531f1cbc1
jabezbekele <jabezbekele@localhost>
2024-01-25 12:42:00 -0600

    done studio1

--- src/studio1/Ordered.java
+++ src/studio1/Ordered.java
@@ -15,7 +15,10 @@ public class Ordered {
 		int y = in.nextInt();
 		System.out.println("Value for z?");
 		int z = in.nextInt();
-
+		Boolean isOrdered = (x > y && y > z || x < y && x < y);
+		
+		System.out.println(isOrdered);
+		
 	}
 
 }
